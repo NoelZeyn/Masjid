17921c11c26c3a3d2be2f1bbad39f0ee206348b5
IStoum <ahmadakrom563@gmail.com>
2025-06-07 13:01:55 +0700

    Hapus

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-// routes/api.php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuizController;
-
-Route::post('/submit-quiz', [QuizController::class, 'store']);
-Route::get('/leaderboard', [QuizController::class, 'leaderboard']);
